22ad9aee2716d2035c3e805af975aa407f10b0e3
ngryman <ngryman@gmail.com>
2017-02-19 18:12:32 +0100

    :beetle: fix pop sift down

--- test/pop.js
+++ test/pop.js
@@ -15,9 +15,25 @@ test('return the top item and remove it', t => {
 
 test('keep best item at the top', t => {
   let { heap } = t.context
-  heap.items = [1, 3, 2]
+  heap.items = [1, 2, 3, 4]
   heap.pop()
-  t.deepEqual(heap.items, [2, 3])
+  t.deepEqual(heap.items, [2, 4, 3])
+})
+
+test('choose the best of both children', t => {
+  let { heap } = t.context
+  heap.items = [1, 4, 2, 8, 16]
+  heap.pop()
+  t.deepEqual(heap.items, [2, 4, 16, 8])
+})
+
+test('handle equal child', t => {
+  let { heap } = t.context
+  heap.items = [1, 2, 8, 4, 16, 16]
+  heap.pop()
+  heap.pop()
+  heap.pop()
+  t.deepEqual(heap.items, [8, 16, 16])
 })
 
 test('return undefined if the heap is empty', t => {
