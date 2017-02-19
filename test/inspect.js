d6292ec2f58d22c74e8de2f41d5ac03de3471834
ngryman <ngryman@gmail.com>
2017-02-19 18:12:59 +0100

    :umbrella: coverage 100%

--- test/inspect.js
+++ test/inspect.js
@@ -1,4 +1,5 @@
 import test from 'ava'
+import util from 'util'
 import BinaryHeap from '../index'
 
 test.beforeEach(t => {
@@ -7,17 +8,34 @@ test.beforeEach(t => {
 
 test('inspect a heap', t => {
   let { heap } = t.context
-  heap.items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3]
+  heap.items = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 9, 8, 7]
   let res = heap.inspect().split('\n').map(l => l.trimRight())
   let expected = [
-    '      ┌────── 1 ──────┐',
-    '  ┌── 2 ──┐       ┌── 3 ──┐',
-    '┌ 4 ┐   ┌ 5 ┐   ┌ 6 ┐     7',
-    '8   9   0   1   2   3',
+    '      ┌────── 9 ──────┐',
+    '  ┌── 8 ──┐       ┌── 7 ──┐',
+    '┌ 6 ┐   ┌ 5 ┐   ┌ 4 ┐     3',
+    '2   1   0   9   8   7',
     ''
   ]
   t.is(res.length, expected.length)
   for (let i = 0; i < res.length; i++) {
-    t.is(res[0], expected[0])
+    t.is(res[i], expected[i])
+  }
+})
+
+test('support colors', t => {
+  let { heap } = t.context
+  heap.items = [2, 1]
+  let res = util.inspect(heap, { colors: true })
+    .split('\n')
+    .map(l => l.trimRight())
+  let expected = [
+    '\u001b[90m┌ \u001b[39m\u001b[33m2\u001b[39m\u001b[90m  \u001b[39m',
+    '\u001b[90m\u001b[39m\u001b[33m1\u001b[39m\u001b[90m\u001b[39m',
+    ''
+  ]
+  t.is(res.length, expected.length)
+  for (let i = 0; i < res.length; i++) {
+    t.is(res[i], expected[i])
   }
 })
--- test/push.js
+++ test/push.js
@@ -15,9 +15,18 @@ test('push an item into the heap', t => {
 test('push items into the heap and keep the better item at the top', t => {
   let { heap } = t.context
   heap.push(4)
-  heap.push(2)
   heap.push(3)
+  heap.push(2)
   heap.push(1)
   t.is(heap.items.length, 4)
   t.deepEqual(heap.items, [1, 2, 3, 4])
+})
+
+test('handle equal items', t => {
+  let { heap } = t.context
+  heap.push(2)
+  heap.push(2)
+  heap.push(1)
+  heap.push(1)
+  t.deepEqual(heap.items, [1, 1, 2, 2])
 })
